638551ead702b2e0a774553a86269dcfb25f16db
Nekitos <nikita.khmyrov@ambiot.io>
2025-01-26 23:57:34 +0300

    - fixed stream_name var

--- start_vlc_demon.bat
+++ start_vlc_demon.bat
@@ -5,7 +5,7 @@ set output_save_folder="E:\VLC_saved_videos\prod"
 set execution_time_in_sec=1800
 set device_name="USB Video Device"
 set server_port=55555
-set stream_name="anyStreamName"
+set stream_name="stream"
 
 :loop
 
@@ -18,8 +18,8 @@ set stream_name="anyStreamName"
 
 	rem --sout-keep option keep open stream connection and conflicts with saving(close) file to filesystem, when we kill task by PID !!!  
 	rem start "VLC Recorder" %vlc_execution_path% dshow:// :dshow-vdev=%device_name% :dshow-adev=none --sout "#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:duplicate{dst=file{dst=%output_file%,no-overwrite},dst=http{mux=ffmpeg{mux=flv},dst=:%server_port%/%anyStreamName%}}" --no-sout-all --sout-keep --run-time=%execution_time_in_sec%
-	start "VLC Recorder" %vlc_execution_path% dshow:// :dshow-vdev=%device_name% :dshow-adev=none --sout "#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:duplicate{dst=file{dst=%output_file%,no-overwrite},dst=http{mux=ffmpeg{mux=flv},dst=:%server_port%/%anyStreamName%}}" --no-sout-all --run-time=%execution_time_in_sec%
-	echo Wait 2 seconds for start proccess...
+	start "VLC Recorder" %vlc_execution_path% dshow:// :dshow-vdev=%device_name% :dshow-adev=none --sout "#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:duplicate{dst=file{dst=%output_file%,no-overwrite},dst=http{mux=ffmpeg{mux=flv},dst=:%server_port%/%stream_name%}}" --no-sout-all --run-time=%execution_time_in_sec%
+	echo Wait 2 seconds for start process...
 	timeout /t 2 >nul
 
 	rem find last created vlc process. Output list default ordering is CreationDate ASC.
